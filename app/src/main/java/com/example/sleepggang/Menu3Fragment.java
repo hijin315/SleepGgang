a4521a668ab2867c100e7e1c93cbc34b6dcd4973
songjuneki <62716528+songjuneki@users.noreply.github.com>
2020-07-02 20:27:12 +0900

    Update Menu3Fragment.java

--- app/src/main/java/com/example/sleepggang/Menu3Fragment.java
+++ app/src/main/java/com/example/sleepggang/Menu3Fragment.java
@@ -14,6 +14,6 @@ public class Menu3Fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu1, container, false);
+        return inflater.inflate(R.layout.fragment_menu3, container, false);
     }
 }
